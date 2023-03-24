628a6c51410d1c4e4d0caae175a5008104c0024b
Felipe Silva <felipemosilvaa@gmail.com>
2023-03-24 20:06:05 -0300

    feat(Home.tsx): Adding qr code lib and passing obj as string

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,27 @@
 import { Column, Text, Button, Input } from 'components'
+import { QRCodeSVG } from 'qrcode.react'
 
 const Home = () => {
+  const obj = {
+    name: '',
+    linkedin: '',
+    github: ''
+  }
+  console.log(JSON.stringify(obj))
   return (
-    <Column>
-      <Text>QR Code Image Generator</Text>
-      <Button>Generate Image</Button>
-      <Input name='chegou' />
+    <Column p='2rem'>
+      <Text variant='big' fontWeight='bold' mb='16px'>
+        QR Code Image Generator
+      </Text>
+      <form>
+        <Input name='name' />
+        <Input name='linkedin' />
+        <Input name='github' />
+        <Button fontWeight='bold' width='340px' mt='16px'>
+          Generate Image
+        </Button>
+      </form>
+      <QRCodeSVG value={JSON.stringify(obj)} />
     </Column>
   )
 }
